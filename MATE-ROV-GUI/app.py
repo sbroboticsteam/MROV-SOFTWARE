5bf81aaf891507e14b51ea6371160743ebf4a46e
Giovanni <giovanni.terefi@stonybrook.edu>
2024-09-27 14:58:21 -0400

    Added comments to app.py

--- MATE-ROV-GUI/app.py
+++ MATE-ROV-GUI/app.py
@@ -3,24 +3,25 @@ from Components.component import Component
 
 import sys
 
-class MainWindow(QMainWindow):
+# each "component" in PyQt5 is a class
+class MainWindow(QMainWindow): # MainWindow class extends QMainWindow
     def __init__(self):
-        super().__init__()
-        self.setWindowTitle("MATE ROV Dashboard")
-        layout = QVBoxLayout()
-        button = QPushButton("Underwater robot go brrr")
-        layout.addWidget(button)
+        super().__init__() # initialize class
+        self.setWindowTitle("MATE ROV Dashboard") # setting the window title (what appears at the top of the window)
+        layout = QVBoxLayout() # QVBoxLayout is used to construct vertical components (i.e. elements are stacked on top of each other)
+        button = QPushButton("Underwater robot go brrr") # QPushButton creates a button that can be clicked
+        layout.addWidget(button) # layout.addWidget adds a widget to the layout
 
-        component = Component()
-        layout.addWidget(component)
+        component = Component() # declaring a new instance of Component()
+        layout.addWidget(component) # adding component to widget (this will appear below the button we declared earlier)
 
-        central = QWidget()
-        central.setLayout(layout)
-        self.setCentralWidget(central)
+        central = QWidget() # create separate widget to act as a central widget (container) for the rest of the widgets
+        central.setLayout(layout) # set the layout for this central widget to the layout we previously defined
+        self.setCentralWidget(central) # set central widget for MainWindow (makes this widget the main content area)
 
-app = QApplication(sys.argv)
+app = QApplication(sys.argv) # required to be called once for every PyQt5 app
 
-window = MainWindow()
-window.show()
+window = MainWindow() # create instance of MainWindow
+window.show() # show the main window (make it visible)
 
-app.exec()
+app.exec() # starts application event loop (keeps app running continuously until we exit)
