8e7c96a6e58a1faa078cfb8137d0358542e9669b
Ada2305 <Ada.Lin.135@gmail.com>
2024-09-28 22:54:26 -0400

    Update camera-> changed to gridlayout instead because it was easier to change dimensions

--- MATE-ROV-GUI/Components/camera.py
+++ MATE-ROV-GUI/Components/camera.py
@@ -1,13 +1,14 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QHBoxLayout, QLabel 
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QHBoxLayout, QLabel, QGridLayout
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPalette, QColor
 
 class Camera(QWidget):
     def __init__(self):
         super().__init__()
-        
+
         layout = QVBoxLayout()
-        label = QLabel("Camera Feeds")
+        
+        label = QLabel(" Camera Feeds")
         label.setMaximumHeight(25)
 
         pal = QPalette();
@@ -17,15 +18,7 @@ class Camera(QWidget):
         label.setPalette(pal);
         layout.addWidget(label)
 
-        background = QWidget();
-        b = QPalette();
-        b.setColor(QPalette.Window, QColor("#D9D9D9"));
-        background.setAutoFillBackground(True);
-        background.setPalette(b)
-        layout.addWidget(background)
-
-        layout_1 = QHBoxLayout()
-        background.setLayout(layout_1)
+        gridlayout = QGridLayout()
 
         label_1 = QLabel("Camera #1")
         label_1.setAlignment(Qt.AlignRight)
@@ -35,21 +28,22 @@ class Camera(QWidget):
         label_1.setAutoFillBackground(True); 
         label_1.setPalette(pal_1);
 
-        layout_1.addWidget(label_1)
+        gridlayout.addWidget(label_1, 0, 0, 2, 2)
 
-        layout_2 = QVBoxLayout()
         label_2 = QLabel("Camera #1")
         label_2.setAlignment(Qt.AlignRight)
         label_2.setPalette(pal_1);
-        label_2.setAutoFillBackground(True);
-        layout_2.addWidget(label_2)
+        label_2.setAutoFillBackground(True); 
+
+        gridlayout.addWidget(label_2, 0, 2)
 
         label_3 = QLabel("Camera #1")
         label_3.setAlignment(Qt.AlignRight)
         label_3.setPalette(pal_1);
-        label_3.setAutoFillBackground(True);
-        layout_2.addWidget(label_3)
+        label_3.setAutoFillBackground(True); 
 
-        layout_1.addLayout(layout_2)
+        gridlayout.addWidget(label_3, 1, 2)
+
+        layout.addLayout(gridlayout)
 
         self.setLayout(layout)
