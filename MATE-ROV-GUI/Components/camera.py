d8dd65d5b091435ac7338efdadc20d3b29a806a0
Ada2305 <Ada.Lin.135@gmail.com>
2024-09-29 00:08:32 -0400

    Updated camera- fixedwidth and fixedheight the labels. Done

--- MATE-ROV-GUI/Components/camera.py
+++ MATE-ROV-GUI/Components/camera.py
@@ -20,29 +20,46 @@ class Camera(QWidget):
 
         gridlayout = QGridLayout()
 
+        box = QWidget()
+        box.setStyleSheet("background-color: white; border: 1px solid lightgray;")
+
         label_1 = QLabel("Camera #1")
         label_1.setAlignment(Qt.AlignRight)
-
         pal_1 = QPalette();
         pal_1.setColor(QPalette.Window, QColor("#B3B3B3"));
         label_1.setAutoFillBackground(True); 
         label_1.setPalette(pal_1);
+        label_1.setFixedWidth(100)
+        label_1.setFixedHeight(25)
 
-        gridlayout.addWidget(label_1, 0, 0, 2, 2)
+        gridlayout.addWidget(box, 0, 0, 4, 4)
+        gridlayout.addWidget(label_1, 0, 3)
+
+        box2 = QWidget()
+        box2.setStyleSheet("background-color: white; border: 1px solid lightgray;")
 
         label_2 = QLabel("Camera #1")
         label_2.setAlignment(Qt.AlignRight)
-        label_2.setPalette(pal_1);
         label_2.setAutoFillBackground(True); 
+        label_2.setPalette(pal_1);
+        label_2.setFixedWidth(100)
+        label_2.setFixedHeight(25)
 
-        gridlayout.addWidget(label_2, 0, 2)
+        gridlayout.addWidget(box2, 0, 4, 2, 2)
+        gridlayout.addWidget(label_2, 0, 5)
+
+        box3 = QWidget()
+        box3.setStyleSheet("background-color: white; border: 1px solid lightgray;")
 
         label_3 = QLabel("Camera #1")
         label_3.setAlignment(Qt.AlignRight)
-        label_3.setPalette(pal_1);
         label_3.setAutoFillBackground(True); 
+        label_3.setPalette(pal_1);
+        label_3.setFixedWidth(100)
+        label_3.setFixedHeight(25)
 
-        gridlayout.addWidget(label_3, 1, 2)
+        gridlayout.addWidget(box3, 2, 4, 2, 2)
+        gridlayout.addWidget(label_3, 2, 5)
 
         layout.addLayout(gridlayout)
 
