1deee101b3221bdcd85d051d256ef4e06f3e556a
BrianASC23 <bcfrags@gmail.com>
2024-10-18 16:52:49 -0400

    just fixed a little thing

--- MATE-ROV-GUI/Components/connectivity.py
+++ MATE-ROV-GUI/Components/connectivity.py
@@ -10,7 +10,7 @@ class Connectivity(QWidget):
         layout = QVBoxLayout()
 
         label = QLabel("Connectivity")
-        label.setStyleSheet("background-color: #555555; color: white; padding: 2.5px;}")
+        label.setStyleSheet("background-color: #555555; color: white; padding: 2.5px")
         label.setFixedHeight(25)
 
         # GroupBox for Connectivity Status Boxes
@@ -20,22 +20,22 @@ class Connectivity(QWidget):
 
         # Raspberry Pi Status
         raspberry_pi = QLabel("Raspberry Pi")
-        raspberry_pi.setStyleSheet("background-color: #297A14; color: white; padding: 10px; border-radius: 15px;}")
+        raspberry_pi.setStyleSheet("background-color: #297A14; color: white; padding: 10px; border-radius: 15px")
         raspberry_pi.setAlignment(QtCore.Qt.AlignCenter)
 
         # Camera 1 Status
         camera1 = QLabel("CAMERA #1")
-        camera1.setStyleSheet("background-color: #9A3413; color: white; padding: 10px; border-radius: 15px;}")
+        camera1.setStyleSheet("background-color: #9A3413; color: white; padding: 10px; border-radius: 15px")
         camera1.setAlignment(QtCore.Qt.AlignCenter)
 
         # Camera 2 Status
         camera2 = QLabel("CAMERA #2")
-        camera2.setStyleSheet("background-color: #9A3413; color: white; padding: 10px; border-radius: 15px;}")
+        camera2.setStyleSheet("background-color: #9A3413; color: white; padding: 10px; border-radius: 15px")
         camera2.setAlignment(QtCore.Qt.AlignCenter)
 
         # X-box Controller Status
         xbox_controller = QLabel("X-box Controller")
-        xbox_controller.setStyleSheet("background-color: #297A14; color: white; padding: 10px; border-radius: 15px;}")
+        xbox_controller.setStyleSheet("background-color: #297A14; color: white; padding: 10px; border-radius: 15px")
         xbox_controller.setAlignment(QtCore.Qt.AlignCenter)
 
         # Add widgets to group box layout
