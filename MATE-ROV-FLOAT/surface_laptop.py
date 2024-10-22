68842088161893eef13b6412f29798763cf6417a
Jerry Lin <103540592+NotJerwee@users.noreply.github.com>
2024-10-21 22:18:31 -0400

    Update surface_laptop.py

--- MATE-ROV-FLOAT/surface_laptop.py
+++ MATE-ROV-FLOAT/surface_laptop.py
@@ -1,11 +1,26 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import requests
+import json
 
 class helloHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write('Hello Conor!'.encode())
+
+    # Store (time, depth) data
+    data = []
+
+    def do_POST(self):
+        if self.path == '/depth':
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length)
+            data = json.loads(body)
+            
+            if 'time' not in data or 'depth' not in data:
+                self.send_response(400)
+                return
+            
+            time = data['time']
+            depth = data['depth']
+            self.data.append((time, depth))
+            self.send_response(200)
 
 def main():
     PORT = 8000
@@ -14,4 +29,4 @@ def main():
     server.serve_forever()
 
 if __name__ == '__main__':
-    main() 
+    main()
