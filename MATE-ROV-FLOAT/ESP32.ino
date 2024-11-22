8f3f0002fd72c2cf562cf68c5b2c4f56a0e8e355
yuetang1 <143581114+YUETANG2@users.noreply.github.com>
2024-11-21 21:41:54 -0500

    Include ip_address field in GET Route

--- MATE-ROV-FLOAT/ESP32.ino
+++ MATE-ROV-FLOAT/ESP32.ino
@@ -2,34 +2,42 @@
 #include <HTTPClient.h>
 #include <WebServer.h>
 
-const char* surface_laptop = "http://192.168.0.177:8000/depth"; 
-
 const char* ssid = "SBRT";
-const char* password =  "Robotic$3";
+const char* password = "Robotic$3";
 int hasStarted = 0; 
 
 // Create a WebServer object on port 80
 WebServer server(80);
 char result[100]; 
+HTTPClient http; 
 
 unsigned long elapsedTime; 
 int depth = 0; 
+String ip_address;
 
 // Function to handle HTTP requests
 void handleToStartSignal() {
-  if(!hasStarted){
-    elapsedTime = millis(); 
-    hasStarted = 1;   
+  if(server.hasArg("ip_address")){
+    ip_address = server.arg("ip_address"); 
+     if(!hasStarted){
+        elapsedTime = millis(); 
+        hasStarted = 1;   
+        http.begin("http://" + ip_address + ":8000/depth");
+        http.addHeader("Content-Type", "text/plain"); 
+        http.setTimeout(5000);
+        server.send(200, "text/plain", "Process starts, will use ip_address: " + ip_address);
+    }else{
+        server.send(400, "text/plain", "Process already started, using ip_address: " + ip_address);
+    }
+  }else{
+    server.send(400, "text/plain", "Missing ip_address");
   }
-
-  Serial.println("Received a request to start the process");
-  server.send(200, "text/plain", "Process starts");
 }
 
 void setup() {
   Serial.begin(115200);  
   WiFi.begin(ssid, password); 
-
+  
   while (WiFi.status() != WL_CONNECTED) 
   { //Check for the connection
     Serial.println("Connecting to WiFi..");
@@ -38,7 +46,7 @@ void setup() {
   
   Serial.println("Connected to the WiFi network");
   Serial.print("IP address is: "); 
-  Serial.print(WiFi.localIP()); 
+  Serial.println(WiFi.localIP()); 
 
   // Define endpoint for GET request
   server.on("/start_signal", HTTP_GET, handleToStartSignal);
@@ -50,10 +58,6 @@ void setup() {
 void loop() {
   if(hasStarted){//insert Xiang's code
     Serial.println("process starts");
-    HTTPClient http; 
-    http.begin(surface_laptop);
-    http.addHeader("Content-Type", "text/plain"); 
-    http.setTimeout(5000);
     
     unsigned long time_elapsed_since = millis() - elapsedTime; 
     int httpResponse = http.POST(String(time_elapsed_since) + "," + String(depth));
@@ -68,7 +72,7 @@ void loop() {
   }else{
    //Serial.println("Process has not start");
   }
-  delay(3000);  //Send a request every 3 seconds
+  delay(3000);  //Send a request every 10 seconds
   // Handle client requests
   server.handleClient();
 }
