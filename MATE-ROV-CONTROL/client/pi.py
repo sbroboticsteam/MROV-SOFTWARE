731f0019dbf182114af3979e415de5df96f6eebc
Adam Feng <adam.feng@stonybrook.edu>
2025-04-05 22:29:14 -0400

    servo code logic

--- MATE-ROV-CONTROL/client/pi.py
+++ MATE-ROV-CONTROL/client/pi.py
@@ -12,35 +12,35 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 's
 # Now you can import get_controller_input
 from controller import get_controller_input
 
-HOST = '192.168.1.130'
+HOST = '192.168.1.133'
 PORT = 4891
 
 gen = get_controller_input()
 print("get_controller_input is callable:", callable(gen))
+while True:
+    print("CALLING FROM CLIENT")
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        # Connect to the Raspberry Pi server
+        client_socket.connect((HOST, PORT))
+        print(f"Connected to server at {HOST}:{PORT}")
+        
+        last_data = None
 
-print("CALLING FROM CLIENT")
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    # Connect to the Raspberry Pi server
-    client_socket.connect((HOST, PORT))
-    print(f"Connected to server at {HOST}:{PORT}")
-    
-    last_data = None
+        # Retrieve controller inputs
+        for inputs in get_controller_input():
+            if inputs != last_data:
+                data = json.dumps(inputs)
+                # data = data.replace('\r\n', '\n')
+                #print(f"Sending: {data}")
+                client_socket.sendall(data.encode('utf-8'))  
+                last_data = inputs 
 
-    # Retrieve controller inputs
-    for inputs in get_controller_input():
-        if inputs != last_data:
-            data = json.dumps(inputs)
-            # data = data.replace('\r\n', '\n')
-            #print(f"Sending: {data}")
-            client_socket.sendall(data.encode('utf-8'))  
-            last_data = inputs 
-
-except Exception as e:
-    print(f"Error: {e}")
-finally:
-    client_socket.close()
-    print("Connection closed.")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        client_socket.close()
+        print("Connection closed.")
 
 # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
